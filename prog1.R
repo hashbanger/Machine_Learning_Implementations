fde059d539c27b848d66e2d1eaf1c97d117eb22b
Prashant Brahmbhatt <prashant.brahmbhatt32@outlook.com>
2018-02-16 00:31:20 +0530

    Delete prog1.R

--- prog1.R
+++
@@ -1,9 +0,0 @@
-#importing the dataset
-dataset=read.csv('Data.csv')
-#changing the missing values to mean of column
-dataset$Age=ifelse(is.na(dataset$Age),ave(dataset$Age, FUN =function(x) mean(x, na.rm=TRUE))
-                   ,dataset$Age)
-
-
-dataset$Salary=ifelse(is.na(dataset$Salary),ave(dataset$Salary, FUN =function(x) mean(x, na.rm=TRUE))
-                   ,dataset$Salary)
