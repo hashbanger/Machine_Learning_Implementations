5835bbdbca2be44bf6d1573e820a8b6793fd5b5a
Prashant Brahmbhatt <prashant.brahmbhatt32@outlook.com>
2018-02-16 01:34:22 +0530

    Adding Categorical Encoding and Dataset Splitting

--- DataPreprocessing/program.py
+++ DataPreprocessing/program.py
@@ -16,3 +16,16 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)#handling Nan to mean along columns
 imputer = imputer.fit(X[:, 1:3])
 X[:,1:3]=imputer.transform(X[:, 1:3])
+
+#Now we are going to encode the categorical data
+from sklearn.preprocessing import LabelEncoder , OneHotEncoder
+labelencoder_X = LabelEncoder()  #encoding it as normal labels just for observation purpose
+X[:,0] = labelencoder_X.fit_transform( X[:,0] )
+onehotencoder= OneHotEncoder(categorical_features = [0]) #encoded the country columns
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()  #encoding it as normal labels just for observation purpose
+y = labelencoder_y.fit_transform(y) #encoding the Yes as 1 and No as 0
+
+#Now Splitting the dataset in test and train
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8)
