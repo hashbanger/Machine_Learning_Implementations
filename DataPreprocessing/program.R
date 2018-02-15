2f438a3b0e3c102c7c1bc06ea1b3b247417c5ad6
Prashant Brahmbhatt <prashant.brahmbhatt32@outlook.com>
2018-02-16 01:57:13 +0530

    Adding encod of categorical data and dataset split

--- DataPreprocessing/program.R
+++ DataPreprocessing/program.R
@@ -7,3 +7,13 @@ dataset$Age=ifelse(is.na(dataset$Age),ave(dataset$Age, FUN =function(x) mean(x, 
 
 dataset$Salary=ifelse(is.na(dataset$Salary),ave(dataset$Salary, FUN =function(x) mean(x, na.rm=TRUE))
                    ,dataset$Salary)
+#Encoding Categorical data
+dataset$Country=factor(dataset$Country,levels=c('France', 'Germany', 'Spain'),labels=c(1,2,3))
+dataset$Purchased=factor(dataset$Purchased,levels=c('No','Yes'),labels=c(0,1))
+
+#Now splitting the data
+#install.packages('caTools')
+library(caTools)
+split=sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
