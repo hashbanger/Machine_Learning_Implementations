46a417a7c8af35e8fe9c64b62affcece872db9a6
Prashant Brahmbhatt <prashant.brahmbhatt32@outlook.com>
2018-02-18 23:10:39 +0530

    Scaled age and salary

--- DataPreprocessing/program.R
+++ DataPreprocessing/program.R
@@ -17,3 +17,7 @@ library(caTools)
 split=sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
+                      
+#feature scaling Age and Salary
+training_set[ ,2:3] = scale(training_set[ ,2:3])
+test_set[, 2:3]= scale(test_set[, 2:3])
