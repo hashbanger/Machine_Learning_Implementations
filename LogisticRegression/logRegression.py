750ff3ca7b3825e25c08be372ad951a7dfa6e1ff
Prashant Brahmbhatt <prashant.brahmbhatt32@outlook.com>
2018-03-17 20:10:50 +0530

    Fixed Plotting Errors

--- LogisticRegression/logRegression.py
+++ LogisticRegression/logRegression.py
@@ -1,35 +1,35 @@
-#implementing Logistic Regression or Classification
-#importing libraries
+#Creating a Logistic Regression Model 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#importing dataset
+#importing dataset 
 dataset = pd.read_csv("Social_Network_Ads.csv")
-X = dataset.iloc[: , 2:4].values
-y = dataset.iloc[:, 4:].values 
+X = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, 4].values
 
 #Splitting the dataset
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-#Scaling the data
+#Scaling the features
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-#importing the classifier
+#fitting Logistic Regression
 from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression()
+classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
-#predicting the results
+#predicting the values
 y_pred = classifier.predict(X_test)
 
-#making the confusion matrix
+#making the confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
@@ -61,7 +61,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression (Training set)')
+plt.title('Logistic Regression (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
